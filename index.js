547c67648b20edb49e41791f728767e3c81f4c8f
Eugene Ware <eugene@noblesamurai.com>
2013-12-30 00:30:11 +1100

    remove MemLively

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var events = require('events'),
 NotFoundError.prototype.notFound = true;
 NotFoundError.prototype.status   = 404;
 
+LivelyDb.NotFoundError = NotFoundError;
+
 module.exports = LivelyDb;
 module.exports.LivelyDb = LivelyDb;
 function LivelyDb() {
@@ -27,36 +29,3 @@ LivelyDb.prototype.put = function (key, value, cb) {
 LivelyDb.prototype.del = function (key, cb) {
   throw new Error('LivelyDb#del() not implemented');
 };
-
-module.exports.MemLively = MemLively;
-function MemLively() {
-  LivelyDb.call(this);
-  this.db = { };
-}
-inherits(MemLively, LivelyDb);
-
-MemLively.prototype.get = function (key, cb) {
-  var self = this;
-  setImmediate(function () {
-    if (typeof self.db[key] === 'undefined') {
-      return cb(new NotFoundError('Key not found in database ' +
-                JSON.stringify(key)));
-    }
-    cb(null, clone(self.db[key]));
-  });
-};
-
-MemLively.prototype.put = function (key, value, cb) {
-  var old = this.db[key];
-  var new_ = clone(value);
-
-  var changes = diff(old, new_);
-  this.db[key] = new_;
-  if (changes.length) this.emit('change', key, changes);
-  setImmediate(cb);
-}
-
-MemLively.prototype.del = function (key, cb) {
-  delete this.db[key];
-  setImmediate(cb);
-}
